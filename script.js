643a34df5b67741814f984544c7bcf0a92f142b4
erick.araujo <erick.araujo@grupoimg.com.br>
2022-05-25 21:59:35 -0300

    Export feature added

--- script.js
+++ script.js
@@ -1,20 +1,59 @@
-const $productName = document.querySelector('#productName')
-const $productPrice = document.querySelector('#productPrice')
-const $productQuantity = document.querySelector('#productQuantity')
+const qs = (selector, parent = document) => {
+    return parent.querySelector(selector)
+}
 
-const $back = document.querySelector('#back')
-const $add = document.querySelector('#add')
+const $exportModal = qs('#exportModal')
+const $export = qs('#export')
+const $confirmExport = qs('#confirmExport')
+const $rejectExport = qs('#rejectExport')
 
-const $productList = document.querySelector('#productList')
+const $productName = qs('#productName')
+const $productPrice = qs('#productPrice')
+const $productQuantity = qs('#productQuantity')
+
+const $back = qs('#back')
+const $add = qs('#add')
+
+const $productList = qs('#productList')
 
 let products = JSON.parse(localStorage.getItem('products')) || []
 
-const productListHeader = "<li><h4><div>➖🛒</div><div>📛</div><div>💲</div><div>±</div></h4></li>";
 
+const closeExportModal = () => {
+    $confirmExport.setAttribute('href', '')
+    $confirmExport.setAttribute('download', '')
+    
+    $exportModal.classList.add('d-none')
+}
+
+const fillConfirmExport = () => {
+    const data = JSON.stringify(products)
+
+    const todayDate = new Date()
+    const dateFormatted = `${todayDate.getDate()}.${todayDate.getMonth()+1}.${todayDate.getFullYear()}`
+
+    $confirmExport.setAttribute('href', 'data:application/json;charset=utf-8,' + encodeURIComponent(data))
+    $confirmExport.setAttribute('download', 'shopping-data-' + dateFormatted + '.json')
+
+    closeExportModal()
+}
+
+const openExportModal = () => {
+    fillConfirmExport()
+    $exportModal.classList.remove('d-none')
+}
+
+const removeItem = (item) => {
+    products.splice(item, 1)
+
+    updateProductList()
+}
+
+const productListHeader = "<li><h4><div>➖🛒</div><div>📛</div><div>💲</div><div>±</div></h4></li>";
 const updateProductList = () => {
     $productList.innerHTML = productListHeader
 
-    for(let item in products) {
+    for (let item in products) {
         const product = products[item]
 
         const $template = document.createElement('template')
@@ -24,12 +63,6 @@ const updateProductList = () => {
     }
 
     localStorage.setItem('products', JSON.stringify(products))
-}
-
-const removeItem = (item) => {
-    products.splice(item, 1)
-
-    updateProductList()
 }
 
 const cleanInputs = () => {
@@ -45,12 +78,15 @@ const addItem = () => {
         quantity: $productQuantity.value
     })
 
-    updateProductList()
     cleanInputs()
+    updateProductList()
 }
 
 $back.onclick = cleanInputs
 
 $add.onclick = addItem
+
+$export.onclick = openExportModal
+$rejectExport.onclick = closeExportModal
 
 updateProductList()
