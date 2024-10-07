663df1f2f3e4cbff4840483355f4ffccc11d234a
Erick Araujo <erickmacena2@gmail.com>
2024-10-06 22:00:56 -0300

    feat: item options mapped

--- src/components/IconItem/IconItem.tsx
+++ src/components/IconItem/IconItem.tsx
@@ -1,19 +1,73 @@
 const iconItems = {
-    "product": {
-        "pt": "Produto",
-        "icon": "🛍️",
-    },
     "add": {
         "pt": "Adicionar",
+        "en": "Add",
         "icon": "➕",
+    },
+    "back": {
+        "pt": "Voltar",
+        "en": "Back",
+        "icon": "◀",
+    },
+    "supermarket": {
+        "pt": "Supermercado",
+        "en": "Supermarket",
+        "icon": "🛒",
+    },
+    "checkout": {
+        "pt": "Finalizar",
+        "en": "Checkout",
+        "icon": "◀🛒",
+    },
+    "product": {
+        "pt": "Produto",
+        "en": "Product",
+        "icon": "🛍️",
+    },
+    "addProduct": {
+        "pt": "Adicionar Produto",
+        "en": "Add Product",
+        "icon": "➕🛍️",
+    },
+    "removeProduct": {
+        "pt": "Remover Produto",
+        "en": "Remove Product",
+        "icon": "➖🛍️",
+    },
+    "totalProducts": {
+        "pt": "Total Produtos",
+        "en": "Total Products",
+        "icon": "🔢🛍️",
+    },
+    "quantity": {
+        "pt": "Quantidade",
+        "en": "Quantity",
+        "icon": "🔢",
+    },
+    "cash": {
+        "pt": "Preço",
+        "en": "Cash",
+        "icon": "💲",
+    },
+    "cancel": {
+        "pt": "Cancelar",
+        "en": "Cancel",
+        "icon": "❌",
     },
     "yes": {
         "pt": "Sim",
-        "icon": "👍"
+        "en": "Yes",
+        "icon": "👍",
     },
     "no": {
         "pt": "Não",
-        "icon": "👎"
+        "en": "No",
+        "icon": "👎",
+    },
+    "export": {
+        "pt": "Exportar",
+        "en": "Export",
+        "icon": "◀🎲",
     },
 } as const
 
